72ae7582d41a777a94bf36948dd175e69eb3f379
Louie Jay Arreza <>
2022-08-18 08:18:55 +0800

    app structure changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'data/provider/network/environment/evironment.dart';
-import 'my_app.dart';
+import 'presentation/my_app.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: Environment.mode);
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_test_project/my_app.dart';
+import 'package:flutter_test_project/presentation/my_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
